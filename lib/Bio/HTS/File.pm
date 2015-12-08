876cebc941376ba51f2b52a8bc66974b3239256e
ah19 <alex.hodgkins@congenica.com>
2015-12-08 10:12:59 +0000

    Add POD

--- lib/Bio/HTS/File.pm
+++ lib/Bio/HTS/File.pm
@@ -1,4 +1,6 @@
-package HTSFile;
+package Bio::HTS::File;
+
+use Bio::HTS;
 
 require Exporter;
 
@@ -6,3 +8,48 @@ our @ISA = qw/Exporter/;
 our @EXPORT = qw/hts_open hts_close/;
 
 1;
+
+__END__
+=head1 NAME
+
+Bio::HTS::File - XS module providing an interface to c htsFile structs
+
+=head1 SYNOPSIS
+
+ use Bio::HTS::File qw(hts_open hts_close);
+
+ my $hts = hts_open("test.bed.gz");
+ 
+ sub DESTROY {
+    hts_close($hts);
+ }
+
+=head1 DESCRIPTION
+
+By itself this module is pretty useless, you'll probably use it with
+other modules under HTS (currently only Tabix exists)
+
+=head2 Methods
+
+=over 12
+
+=item C<hts_open>
+
+Returns a pointer to the htsFile C struct. You must call hts_close
+before the returned pointer goes out of scope or memory won't be freed
+
+=item C<hts_close>
+
+Close a htsFile struct
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright 2015 Congenica Ltd.
+
+=head1 AUTHOR
+
+Alex Hodgkins
+
+=cut
