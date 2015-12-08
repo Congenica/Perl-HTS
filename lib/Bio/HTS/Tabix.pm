64d06f39104cdbde65c9364364b10b0952418142
ah19 <alex.hodgkins@congenica.com>
2015-12-08 12:23:41 +0000

    Move XS loading to HTS.pm, add POD documentation and update to new Bio::HTS namespace

--- lib/Bio/HTS/Tabix.pm
+++ lib/Bio/HTS/Tabix.pm
@@ -1,15 +1,11 @@
-package Tabix;
+package Bio::HTS::Tabix;
 
 use feature qw( say );
 use Mouse;
 
-use TabixIterator;
-use HTSFile qw(hts_open hts_close);
-
-our $VERSION = '0.0.1';
-
-require XSLoader;
-XSLoader::load('Tabix', $VERSION);
+use Bio::HTS; #load the XS
+use Bio::HTS::File qw(hts_open hts_close);
+use Bio::HTS::Tabix::Iterator;
 
 has 'filename' => (
     is       => 'ro',
@@ -86,10 +82,25 @@ sub query {
     my ( $self, $region ) = @_;
 
     die "Please provide a region" unless defined $region;
-    die "Region must be in the format chr:start or chr:start-end" unless $region =~ /[^:]:\d+(?:-(\d+))?/;
+    my ( $chr, $start, $end ) = $region =~ /([^:]):(\d+)(?:-(\d+))?/;
+    unless ( defined $chr and defined $start ) {
+        die "You must specify a region in the format chr:start or chr:start-end";
+    }
+
+    if ( defined $end ) {
+        die "End in $region is less than the start" if $end < $start;
+    }
+    else {
+        say STDERR "Note: You have not specified an end, which actually means chr:start-end_of_chromosome";
+    }
 
     my $iter = tbx_query( $self->_tabix_index, $region );
-    return TabixIterator->new( _tabix_iter => $iter, _htsfile => $self->_htsfile, _tabix_index => $self->_tabix_index );
+
+    unless ( $iter ) {
+        die "Unable to get iterator for region $region -- is your end smaller than your start?";
+    }
+
+    return Bio::HTS::Tabix::Iterator->new( _tabix_iter => $iter, _htsfile => $self->_htsfile, _tabix_index => $self->_tabix_index );
 }
 
 
@@ -114,3 +125,60 @@ sub DEMOLISH {
 1;
 
 __END__
+
+=head1 NAME
+
+Bio::HTS::Tabix - Object oriented access to the underlying tbx C methods
+
+=head1 SYNOPSIS
+
+    use feature qw( say );
+    use Bio::HTS::Tabix;
+
+    my $tabix = Bio::HTS::Tabix->new( filename => "gerp_plus_plus_31July2014.gz" );
+
+    say $tabix->header;
+    my $iter = $tabix->query("1:4000005-4000009");
+
+    while ( my $n = $iter->next ) {
+        say $n;
+    }
+
+=head1 DESCRIPTION
+
+A high level object oriented interface to the htslib tabix (tbx.h) api. Currently it only supports
+retrieving regions from a tabixed file, because that's all I needed it for.
+
+=head2 Methods
+
+=over 12
+
+=item C<header>
+
+Returns all the header lines as a single scalar from the tabixed file
+
+=item C<query>
+
+Takes a single region like: '1:4000005-4000009' or '12:5000000'
+Note: this works exactly the same way as the tabix executable,
+so '12:5000000' actually means get all results from position 5,000,000
+up to the very end of the chromosome. To get results only at position
+5,000,000 you should do '12:5000000-5000001'
+
+Returns a L<Bio::HTS::Tabix::Iterator> for the specified region
+
+=item C<seqnames>
+
+Returns an array ref of chromosomes that are in the indexed file
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright 2015 Congenica Ltd.
+
+=head1 AUTHOR
+
+Alex Hodgkins
+
+=cut
