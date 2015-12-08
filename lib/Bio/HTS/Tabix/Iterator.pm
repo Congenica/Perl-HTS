5ac87e77bddefdcebd2b50e8de354f0eb919f807
ah19 <alex.hodgkins@congenica.com>
2015-12-08 12:24:05 +0000

    Add POD and new namespace

--- lib/Bio/HTS/Tabix/Iterator.pm
+++ lib/Bio/HTS/Tabix/Iterator.pm
@@ -1,8 +1,7 @@
-package TabixIterator;
+package Bio::HTS::Tabix::Iterator;
 
 use Mouse;
-
-require Tabix; #does this actually do anything?
+use Bio::HTS; #load the XS
 
 #this class is just a wrapper around the tabix_iter_next method,
 #all the attributes it needs come from the main Tabix method
@@ -41,4 +40,52 @@ sub DEMOLISH {
 }
 
 1;
+
 __END__
+
+=head1 NAME
+
+Bio::HTS::Tabix::Iterator - XS module wrapping around a tabix hts_itr_t
+
+=head1 SYNOPSIS
+
+You shouldn't be instantiating one of these manually it needs a load of pointers.
+Usage would be through L<Bio::HTS::Tabix>:
+
+    use feature qw( say );
+    use Bio::HTS::Tabix;
+
+    my $tabix = Bio::HTS::Tabix->new( filename => "gerp_plus_plus_31July2014.gz" );
+
+    say $tabix->header;
+    my $iter = $tabix->query("1:4000005-4000009");
+
+    while ( my $n = $iter->next ) {
+        say $n;
+    }
+
+=head1 DESCRIPTION
+
+This is returned from L<Bio::HTS::Tabix>, the only method you need to care about is 'next'.
+
+Don't go importing this and calling new on it if you value your sanity, it won't work.
+
+=head2 Methods
+
+=over 12
+
+=item C<next>
+
+Returns a string with the line from the tabix iterator
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright 2015 Congenica Ltd.
+
+=head1 AUTHOR
+
+Alex Hodgkins
+
+=cut
